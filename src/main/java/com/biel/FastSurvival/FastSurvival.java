ea7fd2df6a9535ea3181bd938e321615a1f38bc7
Biel Simon Rojas <bielsimon2@gmail.com>
2019-12-31 14:47:24 +0100

    Bumped version to 3.9

--- src/main/java/com/biel/FastSurvival/FastSurvival.java
+++ src/main/java/com/biel/FastSurvival/FastSurvival.java
@@ -284,6 +284,7 @@ public final class FastSurvival extends JavaPlugin {
 		if(cmd.getName().equalsIgnoreCase("maze")){ // If the player typed /basic then do the following...
 			if (sender instanceof Player){
 				Player p = (Player) sender;
+				if (!(p.getGameMode() == GameMode.CREATIVE)){p.sendMessage(Utils.L("MUST_BE_IN_CREATIVE"));return true;}
 				int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 8;
 				int y = args.length == 2 ? (Integer.parseInt(args[0])) : 8;
 				MazeGenerator maze = new MazeGenerator(x, y);
